eccd0f59b73dab8712f23935036ea37ef598b16b
Silvio Tomatis <silviot@gmail.com>
2019-02-26 14:18:34 +0100

    Do not use insertMany: it's only supported in mongo 3.2+

--- tests/insert_mongo_test_data.sh
+++ tests/insert_mongo_test_data.sh
@@ -6,11 +6,11 @@ fi
 if [ ! -z "$MONGO_PASSWORD" ] ; then
     MONGO_OPTS="${MONGO_OPTS} -u ${MONGO_PASSWORD}"
 fi
+
+
 SCRIPT='
-db.inventory.insertMany([
-   { item: "journal", qty: 25, tags: ["blank", "red"], size: { h: 14, w: 21, uom: "cm" } },
-   { item: "mat", qty: 85, tags: ["gray"], size: { h: 27.9, w: 35.5, uom: "cm" } },
-   { item: "mousepad", qty: 25, tags: ["gel", "blue"], size: { h: 19, w: 22.85, uom: "cm" } }
-])
+db.inventory.insert({ item: "journal", qty: 25, tags: ["blank", "red"], size: { h: 14, w: 21, uom: "cm" } });
+db.inventory.insert({ item: "mat", qty: 85, tags: ["gray"], size: { h: 27.9, w: 35.5, uom: "cm" } });
+db.inventory.insert({ item: "mousepad", qty: 25, tags: ["gel", "blue"], size: { h: 19, w: 22.85, uom: "cm" } });
 '
 mongo ${MONGO_OPTS} --eval "${SCRIPT}"
