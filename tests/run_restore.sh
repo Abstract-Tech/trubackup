c22f8183ae55887d7bc9829d23a29d0824b96cf3
Silvio Tomatis <silviot@gmail.com>
2019-02-26 18:12:52 +0100

    Fix path to egg in invocation of docker

--- tests/run_restore.sh
+++ tests/run_restore.sh
@@ -2,14 +2,14 @@
 
 SOURCE=$1
 IMAGE=$2
-DIR=$(dirname "$(readlink -f "$0")")
+DIR=$(realpath $(dirname "$(readlink -f "$0")")/../egg)
 
 echo Restoring from ${SOURCE} using ${IMAGE}
 
 set -x
 docker run --network host --rm \
     --mount type=bind,source=${SOURCE},destination=/dumps \
-    --mount type=bind,source=${DIR}/../egg,destination=/egg \
+    --mount type=bind,source=${DIR},destination=/egg \
     ${IMAGE} \
         edxbackup edx_restore \
             --edx-config /egg/edxbackup/tests/lms.auth.restore.json\
