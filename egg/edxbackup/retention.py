3442da8bdd5e732210cc4522c576bf7d6a6c8bc4
Silvio Tomatis <silviot@gmail.com>
2019-08-14 14:56:26 +0200

    Fix doctest

--- egg/edxbackup/retention.py
+++ egg/edxbackup/retention.py
@@ -169,7 +169,7 @@ def to_delete(retention_policy, elements, now=None):
 
 def retention_from_conf(conf):
     """Given a JSON-serializable configuration, return a retention policy.
-    >>> retention_from_conf([({"days": 1}, 8), ({"days": 7}, 4))
-    ((timedelta(1), 8), (timedelta(7), 4))
+    >>> retention_from_conf((({"days": 1}, 8), ({"days": 7}, 4)))
+    {datetime.timedelta(1): 8, datetime.timedelta(7): 4}
     """
     return {timedelta(**el[0]): el[1] for el in conf}
