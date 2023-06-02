5b458d288d2f847e0b2d6c18808800f27e23a698
Illia Shestakov <illia@ishestakov.com>
2023-06-02 12:29:51 +0300

    Backup & restore S3 using s3ball

--- edxbackup/restic/restore.py
+++ edxbackup/restic/restore.py
@@ -22,35 +22,3 @@ def restore_stream(restore_args: list[str], snapshot_id: str, path: str) -> bool
     restore_process.wait()
 
     return (download_process.returncode + restore_process.returncode) == 0
-
-
-def restore_s3(s3_config, snapshot_id: str, path: str) -> bool:
-    s3_client = Minio(
-        s3_config.host,
-        s3_config.access_key,
-        s3_config.secret_key,
-        secure=s3_config.https,
-    )
-
-    download_args = [
-        "restic",
-        "dump",
-        snapshot_id,
-        path,
-    ]
-
-    download_process = Popen(download_args, stdout=PIPE)
-
-    with tarfile.open(mode="r|", fileobj=download_process.stdout) as tar:
-        for tarinfo in tar:
-            if tarinfo.isfile():
-                object_key = tarinfo.name
-
-                ex_reader = tar.extractfile(tarinfo)
-                s3_client.put_object(
-                    s3_config.bucket, object_key, ex_reader, tarinfo.size
-                )
-
-    download_process.wait()
-
-    return download_process.returncode == 0
--- edxbackup/s3.py
+++ edxbackup/s3.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from edxbackup.context import S3Target
-from edxbackup.restic.backup import backup_s3
-from edxbackup.restic.restore import restore_s3
+from edxbackup.restic.backup import backup_stdin
+from edxbackup.restic.restore import restore_stream
 from pydantic import BaseModel
 
 
@@ -12,15 +12,43 @@ class S3Config(BaseModel):
     https: bool
     bucket: str
 
+    def to_backup_options(self) -> list[str]:
+        """
+        Return a Popen args list to be used with mydumper CLI tool
+        """
+        protocol = "https" if self.https else "http"
+        return [
+            f"--endpoint={self.host}",
+            f"--access-key={self.access_key}",
+            f"--secret-key={self.secret_key}",
+            f"--protocol={protocol}",
+            self.bucket,
+        ]
+
+    def to_restore_options(self) -> list[str]:
+        """
+        Return a Popen args list to be used with mydumper CLI tool
+        """
+        protocol = "https" if self.https else "http"
+        return [
+            f"--endpoint={self.host}",
+            f"--access-key={self.access_key}",
+            f"--secret-key={self.secret_key}",
+            f"--protocol={protocol}",
+            self.bucket,
+        ]
+
 
 def dump_s3_bucket(
     s3_config: S3Config, prefix: str, backup_id: str, backup_time: datetime
 ) -> bool:
-    """
-    Dump every object at specified S3 bucket into a directory
-    """
-    return backup_s3(
-        s3_config,
+    backup_args = [
+        "s3ball",
+        "download",
+    ] + s3_config.to_backup_options()
+
+    return backup_stdin(
+        backup_args,
         f"{prefix}_s3",
         backup_id,
         ["s3", f"s3-db:{s3_config.bucket}"],
@@ -29,8 +57,9 @@ def dump_s3_bucket(
 
 
 def restore_s3_bucket(s3_config: S3Config, target: S3Target) -> bool:
-    """
-    Upload every file from .tar archvie into S3 bucket
-    """
-    # FIXME: Handle failure situations
-    return restore_s3(s3_config, target.id, target.path)
+    restore_args = [
+        "s3ball",
+        "upload",
+    ] + s3_config.to_backup_options()
+
+    return restore_stream(restore_args, target.id, target.path)
